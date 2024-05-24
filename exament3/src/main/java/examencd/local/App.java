e0366c487369502eb9567e823856fd48fb324656
Antropodus17 <a23sergiopn@iessanclemente.net>
2024-05-24 10:36:29 +0200

    Creado el md y el proyecto de Maven

---
+++ exament3/src/main/java/examencd/local/App.java
@@ -0,0 +1,13 @@
+package examencd.local;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
